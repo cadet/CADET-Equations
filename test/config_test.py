603947cade03d07356efe73d407bd85e6742b830
Jan Breuer <j.m.br@gmx.de>
2025-04-09 13:43:22 +0200

    Update reference data

--- test/config_test.py
+++ test/config_test.py
@@ -6,7 +6,7 @@
 from streamlit.testing.v1 import AppTest
 import pytest
 
-untested_variables = ["dev_mode", "advanced_mode", "var_format", "param_table", "show_eq_description"] # dev_mode is not tested, TODO: test the advanced_mode
+untested_variables = ["dev_mode", "advanced_mode", "var_format", "param_table", "show_eq_description", "PSD", "PTD"] # dev_mode is not tested, TODO: test the advanced_mode (which includes PSD, PTD)
 
 # Some boxes are conditional, e.g. film_diffusion can only be configured when particles are present.
 # We thus first identify the boxes that change the number of boxes and thus combinations (critical_variables).
