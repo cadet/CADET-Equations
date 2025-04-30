0aa0446c26fd14690f7e1bb0813e4b42ff7eeaa5
Jan Breuer <74359367+jbreue16@users.noreply.github.com>
2025-04-30 23:58:48 +0200

    Mock st.logo in CI to reduce test runtime (#23)

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: jmbr
+"""
+
+import streamlit as st
 import os
 import pytest
+
 
 @pytest.fixture
 def test_dir():
     """Returns the path to the test data directory."""
     return os.path.dirname(__file__)
+
+
+@pytest.fixture(autouse=True)
+def skip_logo(monkeypatch):
+    monkeypatch.setattr(st, "logo", lambda *args, **kwargs: None)
